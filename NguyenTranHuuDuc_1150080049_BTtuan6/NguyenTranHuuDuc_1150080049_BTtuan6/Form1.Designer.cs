e320b4302d32755f45893fe09d6c04db773ac70d
weaseldn <chrisducnguyen04@gmail.com>
2025-10-06 08:32:21 +0700

    Add files via upload

---
+++ NguyenTranHuuDuc_1150080049_BTtuan6/NguyenTranHuuDuc_1150080049_BTtuan6/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace NguyenTranHuuDuc_1150080049_BTtuan6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBunBo = new System.Windows.Forms.Button();
+            this.btnCafe = new System.Windows.Forms.Button();
+            this.btnPepsi = new System.Windows.Forms.Button();
+            this.btnCam = new System.Windows.Forms.Button();
+            this.btn7Up = new System.Windows.Forms.Button();
+            this.btnLipton = new System.Windows.Forms.Button();
+            this.btnCoca = new System.Windows.Forms.Button();
+            this.btnDuiGa = new System.Windows.Forms.Button();
+            this.btnCaVien = new System.Windows.Forms.Button();
+            this.btnKhoaiTay = new System.Windows.Forms.Button();
+            this.btnBanhMy = new System.Windows.Forms.Button();
+            this.btnBurger = new System.Windows.Forms.Button();
+            this.btnMyXao = new System.Windows.Forms.Button();
+            this.btnOcRang = new System.Windows.Forms.Button();
+            this.btnComChien = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbBan = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.dgvOrder = new System.Windows.Forms.DataGridView();
+            this.TenMon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBunBo);
+            this.groupBox1.Controls.Add(this.btnCafe);
+            this.groupBox1.Controls.Add(this.btnPepsi);
+            this.groupBox1.Controls.Add(this.btnCam);
+            this.groupBox1.Controls.Add(this.btn7Up);
+            this.groupBox1.Controls.Add(this.btnLipton);
+            this.groupBox1.Controls.Add(this.btnCoca);
+            this.groupBox1.Controls.Add(this.btnDuiGa);
+            this.groupBox1.Controls.Add(this.btnCaVien);
+            this.groupBox1.Controls.Add(this.btnKhoaiTay);
+            this.groupBox1.Controls.Add(this.btnBanhMy);
+            this.groupBox1.Controls.Add(this.btnBurger);
+            this.groupBox1.Controls.Add(this.btnMyXao);
+            this.groupBox1.Controls.Add(this.btnOcRang);
+            this.groupBox1.Controls.Add(this.btnComChien);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(789, 210);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách món ăn:";
+            // 
+            // btnBunBo
+            // 
+            this.btnBunBo.Location = new System.Drawing.Point(343, 161);
+            this.btnBunBo.Name = "btnBunBo";
+            this.btnBunBo.Size = new System.Drawing.Size(163, 34);
+            this.btnBunBo.TabIndex = 14;
+            this.btnBunBo.Text = "Bún bò Huế";
+            this.btnBunBo.UseVisualStyleBackColor = true;
+            // 
+            // btnCafe
+            // 
+            this.btnCafe.Location = new System.Drawing.Point(431, 114);
+            this.btnCafe.Name = "btnCafe";
+            this.btnCafe.Size = new System.Drawing.Size(75, 41);
+            this.btnCafe.TabIndex = 13;
+            this.btnCafe.Text = "Cafe";
+            this.btnCafe.UseVisualStyleBackColor = true;
+            // 
+            // btnPepsi
+            // 
+            this.btnPepsi.Location = new System.Drawing.Point(334, 114);
+            this.btnPepsi.Name = "btnPepsi";
+            this.btnPepsi.Size = new System.Drawing.Size(75, 41);
+            this.btnPepsi.TabIndex = 12;
+            this.btnPepsi.Text = "Pepsi";
+            this.btnPepsi.UseVisualStyleBackColor = true;
+            // 
+            // btnCam
+            // 
+            this.btnCam.Location = new System.Drawing.Point(431, 73);
+            this.btnCam.Name = "btnCam";
+            this.btnCam.Size = new System.Drawing.Size(75, 35);
+            this.btnCam.TabIndex = 11;
+            this.btnCam.Text = "Cam";
+            this.btnCam.UseVisualStyleBackColor = true;
+            // 
+            // btn7Up
+            // 
+            this.btn7Up.Location = new System.Drawing.Point(334, 74);
+            this.btn7Up.Name = "btn7Up";
+            this.btn7Up.Size = new System.Drawing.Size(75, 34);
+            this.btn7Up.TabIndex = 10;
+            this.btn7Up.Text = "7 Up";
+            this.btn7Up.UseVisualStyleBackColor = true;
+            // 
+            // btnLipton
+            // 
+            this.btnLipton.Location = new System.Drawing.Point(431, 20);
+            this.btnLipton.Name = "btnLipton";
+            this.btnLipton.Size = new System.Drawing.Size(75, 39);
+            this.btnLipton.TabIndex = 9;
+            this.btnLipton.Text = "Lipton";
+            this.btnLipton.UseVisualStyleBackColor = true;
+            // 
+            // btnCoca
+            // 
+            this.btnCoca.Location = new System.Drawing.Point(334, 21);
+            this.btnCoca.Name = "btnCoca";
+            this.btnCoca.Size = new System.Drawing.Size(75, 38);
+            this.btnCoca.TabIndex = 8;
+            this.btnCoca.Text = "Coca";
+            this.btnCoca.UseVisualStyleBackColor = true;
+            // 
+            // btnDuiGa
+            // 
+            this.btnDuiGa.Location = new System.Drawing.Point(185, 161);
+            this.btnDuiGa.Name = "btnDuiGa";
+            this.btnDuiGa.Size = new System.Drawing.Size(130, 34);
+            this.btnDuiGa.TabIndex = 7;
+            this.btnDuiGa.Text = "Đùi gà rán";
+            this.btnDuiGa.UseVisualStyleBackColor = true;
+            // 
+            // btnCaVien
+            // 
+            this.btnCaVien.Location = new System.Drawing.Point(185, 114);
+            this.btnCaVien.Name = "btnCaVien";
+            this.btnCaVien.Size = new System.Drawing.Size(130, 41);
+            this.btnCaVien.TabIndex = 6;
+            this.btnCaVien.Text = "Cá viên chiên";
+            this.btnCaVien.UseVisualStyleBackColor = true;
+            // 
+            // btnKhoaiTay
+            // 
+            this.btnKhoaiTay.Location = new System.Drawing.Point(185, 74);
+            this.btnKhoaiTay.Name = "btnKhoaiTay";
+            this.btnKhoaiTay.Size = new System.Drawing.Size(130, 34);
+            this.btnKhoaiTay.TabIndex = 5;
+            this.btnKhoaiTay.Text = "Khoai tây chiên";
+            this.btnKhoaiTay.UseVisualStyleBackColor = true;
+            // 
+            // btnBanhMy
+            // 
+            this.btnBanhMy.Location = new System.Drawing.Point(185, 20);
+            this.btnBanhMy.Name = "btnBanhMy";
+            this.btnBanhMy.Size = new System.Drawing.Size(130, 39);
+            this.btnBanhMy.TabIndex = 4;
+            this.btnBanhMy.Text = "Bánh mì ốp la";
+            this.btnBanhMy.UseVisualStyleBackColor = true;
+            // 
+            // btnBurger
+            // 
+            this.btnBurger.Location = new System.Drawing.Point(13, 157);
+            this.btnBurger.Name = "btnBurger";
+            this.btnBurger.Size = new System.Drawing.Size(142, 38);
+            this.btnBurger.TabIndex = 3;
+            this.btnBurger.Text = "Burger bò nướng";
+            this.btnBurger.UseVisualStyleBackColor = true;
+            // 
+            // btnMyXao
+            // 
+            this.btnMyXao.Location = new System.Drawing.Point(13, 110);
+            this.btnMyXao.Name = "btnMyXao";
+            this.btnMyXao.Size = new System.Drawing.Size(142, 41);
+            this.btnMyXao.TabIndex = 2;
+            this.btnMyXao.Text = "Mì xào hải sản";
+            this.btnMyXao.UseVisualStyleBackColor = true;
+            // 
+            // btnOcRang
+            // 
+            this.btnOcRang.Location = new System.Drawing.Point(13, 65);
+            this.btnOcRang.Name = "btnOcRang";
+            this.btnOcRang.Size = new System.Drawing.Size(142, 39);
+            this.btnOcRang.TabIndex = 1;
+            this.btnOcRang.Text = "Ốc rang muối";
+            this.btnOcRang.UseVisualStyleBackColor = true;
+            // 
+            // btnComChien
+            // 
+            this.btnComChien.Location = new System.Drawing.Point(13, 21);
+            this.btnComChien.Name = "btnComChien";
+            this.btnComChien.Size = new System.Drawing.Size(142, 38);
+            this.btnComChien.TabIndex = 0;
+            this.btnComChien.Text = "Cơm chiên trứng";
+            this.btnComChien.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbBan);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnOrder);
+            this.groupBox2.Controls.Add(this.btnXoa);
+            this.groupBox2.Location = new System.Drawing.Point(-1, 299);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(535, 69);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // cbBan
+            // 
+            this.cbBan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBan.FormattingEnabled = true;
+            this.cbBan.Items.AddRange(new object[] {
+            "Bàn 1",
+            "Bàn 2",
+            "Bàn 3",
+            "Bàn 4"});
+            this.cbBan.Location = new System.Drawing.Point(235, 27);
+            this.cbBan.Name = "cbBan";
+            this.cbBan.Size = new System.Drawing.Size(155, 24);
+            this.cbBan.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(162, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chọn bàn:";
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(397, 12);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(109, 42);
+            this.btnOrder.TabIndex = 1;
+            this.btnOrder.Text = "Order";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(29, 12);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(111, 42);
+            this.btnXoa.TabIndex = 0;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // dgvOrder
+            // 
+            this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrder.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TenMon,
+            this.SoLuong});
+            this.dgvOrder.Location = new System.Drawing.Point(12, 375);
+            this.dgvOrder.Name = "dgvOrder";
+            this.dgvOrder.RowHeadersWidth = 51;
+            this.dgvOrder.RowTemplate.Height = 24;
+            this.dgvOrder.Size = new System.Drawing.Size(511, 150);
+            this.dgvOrder.TabIndex = 4;
+            // 
+            // TenMon
+            // 
+            this.TenMon.HeaderText = "Tên món";
+            this.TenMon.MinimumWidth = 6;
+            this.TenMon.Name = "TenMon";
+            this.TenMon.Width = 125;
+            // 
+            // SoLuong
+            // 
+            this.SoLuong.HeaderText = "Số Lượng";
+            this.SoLuong.MinimumWidth = 6;
+            this.SoLuong.Name = "SoLuong";
+            this.SoLuong.Width = 125;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(155, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(379, 85);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quán ăn nhanh Hưng Thịnh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvOrder);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCafe;
+        private System.Windows.Forms.Button btnPepsi;
+        private System.Windows.Forms.Button btnCam;
+        private System.Windows.Forms.Button btn7Up;
+        private System.Windows.Forms.Button btnLipton;
+        private System.Windows.Forms.Button btnCoca;
+        private System.Windows.Forms.Button btnDuiGa;
+        private System.Windows.Forms.Button btnCaVien;
+        private System.Windows.Forms.Button btnKhoaiTay;
+        private System.Windows.Forms.Button btnBanhMy;
+        private System.Windows.Forms.Button btnBurger;
+        private System.Windows.Forms.Button btnMyXao;
+        private System.Windows.Forms.Button btnOcRang;
+        private System.Windows.Forms.Button btnComChien;
+        private System.Windows.Forms.Button btnBunBo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.DataGridView dgvOrder;
+        private System.Windows.Forms.ComboBox cbBan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenMon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuong;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NguyenTranHuuDuc_1150080049_BTtuan6/NguyenTranHuuDuc_1150080049_BTtuan6/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NguyenTranHuuDuc_1150080049_BTtuan6
+{
+    public partial class Form1 : Form
+    {
+        // Danh sách món đã chọn (tên món -> số lượng)
+        private Dictionary<string, int> orderList = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // đảm bảo Form1_Load được gọi (tránh trường hợp Designer chưa gán)
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // kiểm tra control cơ bản có tồn tại không
+            if (cbBan == null || dgvOrder == null)
+            {
+                MessageBox.Show("Không tìm thấy cbBan hoặc dgvOrder. Vào Designer kiểm tra tên (Name) của các control.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Thêm bàn vào ComboBox
+            cbBan.Items.Clear();
+            cbBan.Items.AddRange(new string[] { "Bàn 1", "Bàn 2", "Bàn 3", "Bàn 4" });
+            if (cbBan.Items.Count > 0) cbBan.SelectedIndex = 0;
+
+            // Cấu hình DataGridView (tạo cột)
+            dgvOrder.SuspendLayout();
+            dgvOrder.Columns.Clear();
+
+            var colTenMon = new DataGridViewTextBoxColumn
+            {
+                Name = "TenMon",
+                HeaderText = "Tên món",
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+                ReadOnly = true
+            };
+            var colSoLuong = new DataGridViewTextBoxColumn
+            {
+                Name = "SoLuong",
+                HeaderText = "Số lượng",
+                Width = 80,
+                ReadOnly = true
+            };
+
+            dgvOrder.Columns.Add(colTenMon);
+            dgvOrder.Columns.Add(colSoLuong);
+
+            dgvOrder.AllowUserToAddRows = false;
+            dgvOrder.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            // programmatic adding vẫn hoạt động tốt; giữ ReadOnly = false để an toàn nếu cần chỉnh chương trình
+            dgvOrder.ReadOnly = false;
+            dgvOrder.ResumeLayout();
+
+            // Gán sự kiện cho các Button món ăn (an toàn: nếu button không tồn tại thì bỏ qua)
+            AttachButtonHandler("btnComChien", (s, ev) => AddItem("Cơm chiên trứng"));
+            AttachButtonHandler("btnBanhMy", (s, ev) => AddItem("Bánh mì ốp la"));
+            AttachButtonHandler("btnCoca", (s, ev) => AddItem("Coca"));
+            AttachButtonHandler("btnLipton", (s, ev) => AddItem("Lipton"));
+            AttachButtonHandler("btnOcRang", (s, ev) => AddItem("Ốc rang muối"));
+            AttachButtonHandler("btnKhoaiTay", (s, ev) => AddItem("Khoai tây chiên"));
+            AttachButtonHandler("btn7Up", (s, ev) => AddItem("7 Up"));
+            AttachButtonHandler("btnCam", (s, ev) => AddItem("Cam"));
+            AttachButtonHandler("btnMyXao", (s, ev) => AddItem("Mỳ xào hải sản"));
+            AttachButtonHandler("btnCaVien", (s, ev) => AddItem("Cá viên chiên"));
+            AttachButtonHandler("btnPepsi", (s, ev) => AddItem("Pepsi"));
+            AttachButtonHandler("btnCafe", (s, ev) => AddItem("Cafe"));
+            AttachButtonHandler("btnBurger", (s, ev) => AddItem("Burger bò nướng"));
+            AttachButtonHandler("btnDuiGa", (s, ev) => AddItem("Đùi gà rán"));
+            AttachButtonHandler("btnBunBo", (s, ev) => AddItem("Bún bò Huế"));
+
+            // Nút chức năng
+            AttachButtonHandler("btnXoa", BtnXoa_Click);
+            AttachButtonHandler("btnOrder", BtnOrder_Click);
+        }
+
+        // Gán event cho Button an toàn (tìm control theo tên trong tất cả các container)
+        private void AttachButtonHandler(string controlName, EventHandler handler)
+        {
+            var found = this.Controls.Find(controlName, true);
+            if (found.Length == 0) return; // nếu không thấy, bỏ qua (tránh crash)
+            if (found[0] is Button btn)
+            {
+                // loại bỏ handler trùng lặp (an toàn khi load lại)
+                btn.Click -= handler;
+                btn.Click += handler;
+            }
+        }
+
+        // Hàm thêm món
+        private void AddItem(string tenMon)
+        {
+            // debug tạm: bỏ dòng MessageBox sau khi chắc chắn hoạt động
+            // MessageBox.Show("AddItem: " + tenMon);
+
+            if (orderList.ContainsKey(tenMon))
+                orderList[tenMon]++;
+            else
+                orderList[tenMon] = 1;
+
+            LoadDataToGrid();
+        }
+
+        // Hiển thị danh sách món trên DataGridView
+        private void LoadDataToGrid()
+        {
+            if (dgvOrder == null) return;
+            dgvOrder.Rows.Clear();
+            foreach (var item in orderList)
+            {
+                dgvOrder.Rows.Add(item.Key, item.Value);
+            }
+        }
+
+        // Xóa món đang chọn
+        private void BtnXoa_Click(object sender, EventArgs e)
+        {
+            if (dgvOrder == null) return;
+
+            if (dgvOrder.SelectedRows.Count > 0)
+            {
+                var cell = dgvOrder.SelectedRows[0].Cells["TenMon"].Value;
+                if (cell != null)
+                {
+                    string tenMon = cell.ToString();
+                    if (orderList.ContainsKey(tenMon))
+                    {
+                        orderList.Remove(tenMon);
+                        LoadDataToGrid();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn 1 món để xóa.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Ghi đơn hàng xuống file txt
+        private void BtnOrder_Click(object sender, EventArgs e)
+        {
+            if (orderList.Count == 0)
+            {
+                MessageBox.Show("Chưa chọn món nào!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string ban = cbBan.SelectedItem?.ToString() ?? cbBan.Text ?? "Unknown";
+            string fileName = $"Order_{ban}_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    sw.WriteLine($"Bàn: {ban}");
+                    sw.WriteLine("Danh sách món ăn:");
+                    foreach (var item in orderList)
+                    {
+                        sw.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                }
+
+                MessageBox.Show($"Đã gửi order xuống bếp (ghi file {fileName}).", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                orderList.Clear();
+                LoadDataToGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi ghi file: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
